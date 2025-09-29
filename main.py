5fbf3bd453a5247c2f4f190da66178da23a61bb9
Fenin_Jose <123000656+Fenin76@users.noreply.github.com>
2025-09-29 22:25:00 +0200

    Clean up main.py by removing comments and whitespace
    Removed unnecessary comments and whitespace in main loop.

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ pill = player()
 
 #mainloop
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+   
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
@@ -23,9 +22,9 @@ while running:
     if keys[pg.K_RIGHT] and (pill.x_loc <= 520):
          pill.move_right()
 
-    # flip() the display to put your work on screen
 
     pill.bg_update()
     clock.tick(60)  # limits FPS to 60
+
 
 pg.quit()
